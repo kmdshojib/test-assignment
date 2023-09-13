daa453ee5bf9ed128f62f7bc7d2b285cb62fde94
kmdshojib <kmd.shojib@gmail.com>
2023-09-13 15:53:19 +0600

    init class has been created

--- core/init.php
+++ core/init.php
@@ -11,16 +11,23 @@ class Config implements ConfigInterface
 
     public static function getSiteRoot()
     {
-
+        return self::getDs() . "xampp" . self::getDs() . "htdocs" . self::getDs() . "scadi-test";
     }
 
     public static function getIncPath()
     {
-
+        return self::getSiteRoot() . self::getDs() . "includes";
     }
 
     public static function getCorePath()
     {
-
+        return self::getSiteRoot() . self::getDs() . "core";
     }
 }
+
+$ds = Config::getDs();
+$siteRoot = Config::getSiteRoot();
+$incPath = Config::getIncPath();
+$corePath = Config::getCorePath();
+
+require_once $incPath . "/config.php";

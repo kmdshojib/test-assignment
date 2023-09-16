aefda10a3184569ebc2dbe85afdc67e87268355d
kmdshojib <kmd.shojib@gmail.com>
2023-09-17 01:10:37 +0600

    form has been styled

--- frontend/src/Pages/AddProduct.jsx
+++ frontend/src/Pages/AddProduct.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { addProdduct, getCategories } from '../api/apiRequests';
 import Form from 'react-bootstrap/Form';
+import "../styles/addproduct.scss"
 
 const AddProduct = () => {
     const navigate = useNavigate();
@@ -40,72 +41,80 @@ const AddProduct = () => {
     };
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <div>
-                        <label htmlFor="sku">Sku</label>
-                        <input type="text" name="sku" {...register("sku")} />
-                    </div>
+            <div >
+                <form className='d-flex justify-content-center mt-3' onSubmit={handleSubmit(onSubmit)}>
+                    <div className='form-container'>
+                        <div className='input-container'>
+                            <label className='input-label' htmlFor="sku">Sku:</label>
+                            <input className='form-input' type="text" name="sku" placeholder='#SKU' {...register("sku")} />
+                        </div>
 
-                    <div>
-                        <label htmlFor="sku">Name</label>
-                        <input type="text" name="name" {...register("name")} />
-                    </div>
+                        <div className='input-container'>
+                            <label className='input-label' htmlFor="name">Name: </label>
+                            <input className='form-input' type="text" name="name" placeholder='#Name' {...register("name")} />
+                        </div>
 
-                    <div>
-                        <label htmlFor="price">price</label>
-                        <input type="number" name="price" {...register("price")} />
-                    </div>
+                        <div className='input-container'>
+                            <label className='input-label' htmlFor="price">Price:</label>
+                            <input className='form-input' type="number" name="price" placeholder='#Price' {...register("price")} />
+                        </div>
 
 
-                    <Form.Select id="type" onChange={handleTypeChange} required>
-                        <option value=""> --Type Switcher-- </option>
-                        {data?.data &&
-                            data.data.map((item, index) => (
-                                <React.Fragment key={index}>
-                                    <option>{item.dvd}</option>
-                                    <option>{item.book}</option>
-                                    <option>{item.furniture}</option>
-                                </React.Fragment>
-                            ))
+                        <Form.Select className='form-option' id="type" onChange={handleTypeChange} required>
+                            <option value=""> --Type Switcher-- </option>
+                            {data?.data &&
+                                data.data.map((item, index) => (
+                                    <React.Fragment key={index}>
+                                        <option>{item.dvd}</option>
+                                        <option>{item.book}</option>
+                                        <option>{item.furniture}</option>
+                                    </React.Fragment>
+                                ))
+                            }
+                        </Form.Select>
+
+                        {
+                            type === "DVD" && (
+                                <div className='input-container'>
+                                    <label className='input-label' htmlFor='size'>Size(MB): </label>
+                                    <input className='form-input' type='number' name='size' placeholder='#Size' required {...register("size")} />
+                                </div>
+                            )
                         }
-                    </Form.Select>
+                        {
+                            type === "Book" && (
+                                <div className='input-container'>
+                                    <label className='input-label' htmlFor='weight'>Weight(KG): </label>
+                                    <input className='form-input' type='number' name='weight' placeholder='#Weight' required {...register("weight")} />
+                                </div>
+                            )
+                        }
+                        {
+                            type === "Furniture" && (
+                                <div>
+                                    <div className='input-container'>
+                                        <label className='input-label' htmlFor='height'>Height(CM): </label>
+                                        <input className='form-input' id='height' type='number' name='height' placeholder='#Height' required {...register("height")} />
+                                    </div>
 
-                    {
-                        type === "DVD" && (
-                            <div>
-                                <label htmlFor='size'>Size(MB): </label>
-                                <input type='number' name='size' placeholder='#size' required {...register("size")} />
-                            </div>
-                        )
-                    }
-                    {
-                        type === "Book" && (
-                            <div>
-                                <label htmlFor='weight'>Weight(KG): </label>
-                                <input type='number' name='weight' placeholder='#weight' required {...register("weight")} />
-                            </div>
-                        )
-                    }
-                    {
-                        type === "Furniture" && (
-                            <div>
-                                <label htmlFor='height'>Height(CM): </label>
-                                <input id='height' type='number' name='height' placeholder='#Height' required {...register("height")} />
+                                    <div className='input-container'>
+                                        <label className='input-label' htmlFor='width'>Width(CM): </label>
+                                        <input className='form-input' type='number' name='width' placeholder='#Width' required {...register("width")} />
+                                    </div>
 
-                                <label htmlFor='width'>Width(CM): </label>
-                                <input type='number' name='width' placeholder='#Width' required {...register("width")} />
+                                    <div className='input-container'>
+                                        <label className='input-label' htmlFor='length'>Length(CM): </label>
+                                        <input className='form-input' type='number' name='length' placeholder='#Length' required {...register("length")} />
 
-                                <label htmlFor='length'>Length(CM): </label>
-                                <input type='number' name='length' placeholder='#length' required {...register("length")} />
+                                    </div>
+                                </div>
+                            )
+                        }
 
-                            </div>
-                        )
-                    }
-
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+                        <button className='button mt-3 rounded btn btn-light border border-dark' type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }

7a146803afa02c16a5ae0d6872111dfd4d6815d1
kmdshojib <kmd.shojib@gmail.com>
2023-09-17 16:25:58 +0600

    array for ids has been made

--- frontend/src/Components/ProductCard.jsx
+++ frontend/src/Components/ProductCard.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import "../styles/card.styles.scss"
 
-const ProductCard = ({ sku, name, price, children, onChange, value, id }) => {
+const ProductCard = ({ sku, name, price, children, isChecked, id, onCardClick }) => {
+
+    const handleCheck = () => {
+        if (onCardClick) {
+            onCardClick(id);
+        }
+    };
+
     return (
-        <div key={id} className='card-container card'>
+        <div onClick={handleCheck} key={id} className='card-container card'>
             <div className='d-flex m-2'>
-                <input type='checkbox' onChange={onChange} value={value} />
+                <input
+                    type='checkbox'
+                    checked={isChecked || false}
+                    readOnly
+                />
             </div>
             <div className='card-body d-flex flex-column'>
                 <h5 className="card-title">{sku}</h5>
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useQuery } from "@tanstack/react-query"
 import { getProducts } from '../api/apiRequests'
 import ProductCard from '../Components/ProductCard'
-
+import "../styles/home.styles.scss"
 
 
 const Home = () => {
 
     const { data, isLoading } = useQuery({ queryKey: ["products"], queryFn: getProducts })
-
+    const [ids, setIds] = useState([])
+    const [checkedStates, setCheckedStates] = useState(Array.from({ length: data?.data.length }, () => false));
     if (isLoading) {
         return <div>isLoading..</div>
     }
 
-    console.log(data)
-    const handleChange = (e) => {
-        console.log(e.target.value)
-    }
+    const handleCardClick = (index, id) => {
+        const updatedStates = [...checkedStates];
+        updatedStates[index] = !updatedStates[index];
+        setCheckedStates(updatedStates);
+
+        if (updatedStates[index]) {
+            setIds(preValue => [...preValue, id]);
+        } else {
+            setIds(preValue => preValue.filter(item => item !== id));
+        }
+    };
+    console.log(ids)
     return (
         <div className='product-container'>
             {
-                data.data.map(item => (
+                data.data.map((item, index) => (
                     <ProductCard
                         key={item.id}
-                        onChange={handleChange}
                         value={item.id}
                         sku={item.sku}
                         name={item.name}
                         price={item.price}
+                        isChecked={checkedStates[index]}
+                        onCardClick={() => handleCardClick(index, item.id)}
                     >
                         {
                             item.size && <div>Size:{item.size}MB</div>
